45e83d021a35d8a96a22f2914ca20015904726db
shubhpro <shubhpro@shubhams-MacBook-Pro.local>
2022-08-01 20:03:31 +0530

    js code

---
+++ julybatchjs/script.js
@@ -0,0 +1,92 @@
+//You can write js code in here 
+// the output of your js code is shown in the browser console
+
+console.log("hello world") ; // this will print hello world in my browser console
+
+
+/**
+ * Multiline comment like this
+ */
+
+/**
+ * Variable declaration
+ * the variable stores data 
+ * we need to store data to perform logical operations
+ */
+
+//var let and const ----keywords for variable declaration
+
+var sunnydeol='you pets of balwant rai';  // string //text 
+console.log(sunnydeol);
+
+let johncena="You cant see mee";
+console.log(johncena);
+
+let num=9;
+let num2=92;
+let sum=num+num2;
+let multiply=num*num2; // * -->sign for multiplication
+
+console.log(sum);
+console.log(multiply);
+// operators are something which do the logical operations between two variables
+//Division has two segments -- quotient and remainder
+//for quotient
+let quotient=num2/num; //   / -->is used to get the quotient
+let remainder=num2%num;  //   % (modulus operator) --->is used to get the remainder
+
+console.log("Quotient---",quotient);
+console.log("Remainder---",remainder);
+
+
+// Anything which is in double quotes or single quotes is string in js 
+
+//functions  --->
+
+var sameer=90;
+let a=9;  
+let b=9;
+var k="hello";
+var k="jeueuue";
+console.log(k);
+let c=true;
+var stringnumber="8999"; // this becomes string
+var number=999900;// this is a number
+
+console.log(sameer);
+
+
+//functions--->
+//keyword
+function myFunction(){  //name of function  
+   // the logic of function goes in here --->
+
+
+
+
+
+}
+
+// sum of numbers -->
+var a1=1830;
+var a2=1930;
+console.log(a1+a2);
+
+function sumOfNumbers(a,b){ // a,b are parameters
+    var sum=a+b;
+
+    console.log(sum);
+
+}
+sumOfNumbers(9,90); // 9,90 arguments
+sumOfNumbers(90,9000);
+sumOfNumbers(19,190)
+
+//Assignment time 
+
+//Write four functions that takes two parameters
+// one will do addition of numbers
+// one will do subtraction
+// one will do multiplication
+// one will do quotient
+// one will do remainder;
