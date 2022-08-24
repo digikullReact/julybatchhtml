425827cfaf861c7db776c1c3f99214276c0c54ab
Shubham Dixit <shubhamdixit863@gmail.com>
2022-08-24 19:19:12 +0530

    Create queryselectorexample.js

---
+++ queryselectorexample.js
@@ -0,0 +1,16 @@
+//const headings = document.getElementsByTagName('h1');
+/*
+[...headings].forEach((ele) => {
+  ele.style.color = 'red';
+});
+*/
+/*
+const selected = document.querySelector('.div>h1');
+
+selected.style.color = 'brown';
+*/
+/*
+const h1 = document.createElement('h1');
+h1.textContent = 'jjjjj';
+document.getElementById('div').appendChild(h1);
+*/
